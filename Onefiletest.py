21fa07ce9f7e63c21b6f2a01bf2578843f1b4e6d
Cyrusr2 <98433506+Cyrusr2@users.noreply.github.com>
2022-03-15 20:40:57 -0400

    Update Onefiletest.py

--- Onefiletest.py
+++ Onefiletest.py
@@ -29,30 +29,16 @@ uploaded_file=st.file_uploader("Upload DNA Sequence Here", type=['txt'], accept_
 
 
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+   dataframe = pd.read_csv(uploaded_file)
+   st.write(dataframe)
     
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file, delimiter=",")
-    st.write(dataframe)
-    
-    uploadbutton=st.button('Upload',disabled= False)
-    
-    if uploadbutton:
-       st.write('Uploading(add progress bar here)') 
+   uploadbutton=st.button('Upload',disabled= False)
+     
        
 if st.button('Predict!'):
-    df_json = dataframe.to_json(orient='records', date_format='iso') 
-    y = model.predict(df_json)
+    df = pd.DataFrame(dataframe.dict(), index=[30])
+    df['RPPA_Y2K'], df['RPPA_YZY'] = [1.065743, 0.693467]
+    y = model.predict(df)
     y = [0 if val < 0.5 else 1 for val in y]
     if y == 1:
         survival = 'You will survive.'
